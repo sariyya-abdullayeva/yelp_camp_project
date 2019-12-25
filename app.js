99fb6c4c5022bc4d600c630d7c8900daef64b584
codeforphilosophy <sariyya.abdullayeva@gmail.com>
2019-12-25 20:26:18 +0200

    Header/'Navbar', New.ejs/'Form design',Footer.ejs/'p del.'

--- app.js
+++ app.js
@@ -14,9 +14,16 @@ app.use(bodyParser.urlencoded({extended:true}));
 // <%- include("partials/footer") %>
 
 var campGrounds = [
-    {name:'Salmon Creek', image:'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=649&q=80'},
+    // {name:'Salmon Creek', image:'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=649&q=80'},
+    {name:'Setoma Village', image:'https://images.unsplash.com/photo-1455496231601-e6195da1f841?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
     {name:'Sarema Island', image:'https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
+    // {name:'Salmon Creek', image:'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=649&q=80'},
+    {name:'Sarema Island', image:'https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
+    // {name:'Sarema Island', image:'https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
+    // {name:'Sarema Island', image:'https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
+    {name:'Setoma Village', image:'https://images.unsplash.com/photo-1455496231601-e6195da1f841?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
     {name:'Setoma Village', image:'https://images.unsplash.com/photo-1455496231601-e6195da1f841?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'}
+
 ]
 
 
@@ -42,7 +49,7 @@ app.post('/campgrounds', function(req, res){
     
     //and add to campGround
     var newCampGround = {
-        name: name ,
+        name: name,
         image: image
     };
 
@@ -54,7 +61,7 @@ app.post('/campgrounds', function(req, res){
 
 
 //submiting form that will send a data to post/campgrounds route
-app.get('/camogrounds/new', function(req, res){
+app.get('/campgrounds/new', function(req, res){
     res.render('new.ejs');
 });
 

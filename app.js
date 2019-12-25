15a9fbcc0b99c87370a8b8f4ab956398fbee9e37
codeforphilosophy <sariyya.abdullayeva@gmail.com>
2019-12-25 14:29:07 +0200

    Add Landing.ejs, Campground page, name, img

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require("express")
+var app= express ();
+
+app.set("view engine", "ejs");
+
+
+
+app.get("/", function(req, res){
+    res.render("landing");
+})
+
+app.get('/campgrounds', function(req, res){
+    var campGrounds = [
+        {name:'Salmon Creek', image:'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=649&q=80'},
+        {name:'Sarema Island', image:'https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
+        {name:'Setoma Village', image:'https://images.unsplash.com/photo-1455496231601-e6195da1f841?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'}
+    ]
+    res.render("campgrounds", {campgrounds:campGrounds})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("yelp_camp app started", );
+});

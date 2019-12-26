31ae38936c248d9ec70c3221748599a7bbd8d9e4
codeforphilosophy <sariyya.abdullayeva@gmail.com>
2019-12-26 10:32:28 +0200

    Add: Mongoose to app.js, created first data collec. campgrounds

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var express = require("express")
-var app= express ();
+var express    = require("express")
+var app        = express ();
 //to make post requests
 var bodyParser = require("body-parser");
+//For DB
+var mongoose   = require("mongoose");
+
+
 
 //tell express use ejs as a style for page adresses
 app.set("view engine", "ejs");
@@ -9,9 +13,36 @@ app.set("view engine", "ejs");
 //tell express to use body-parser
 app.use(bodyParser.urlencoded({extended:true}));
 
+//Connect DB to Express
+mongoose.connect("mongodb://localhost/yelp_app", {useNewUrlParser: true, useUnifiedTopology: true });
+
 //to include header and footer tepmlates to other pages
 // <%- include("partials/header") %>
 // <%- include("partials/footer") %>
+
+
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+})
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+Campground.create(
+    {
+        name:'Setoma Village', 
+        image:'https://images.unsplash.com/photo-1455496231601-e6195da1f841?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    
+    }, function(err, camp){
+        if(err){
+            console.log(err)
+        }else{
+            console.log("OKAY")
+            console.log(camp)
+        }
+    }
+
+)
 
 var campGrounds = [
     // {name:'Salmon Creek', image:'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=649&q=80'},
